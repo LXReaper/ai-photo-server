bc766f567a426b3651935000047b722f0bc4363c
yyp <yyp1326126225@gmail.com>
2025-07-22 14:14:58 +0800

    first commit

---
+++ src/main/java/com/yyp/aiphotoservice/AiPhotoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.yyp.aiphotoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiPhotoServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiPhotoServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yyp/aiphotoservice/AiPhotoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yyp.aiphotoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiPhotoServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
